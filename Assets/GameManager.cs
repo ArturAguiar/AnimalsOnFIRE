e61a18f63a0031d6f4961759d38969b5bcd7b36d
Danny Carpenter <djc423@cornell.edu>
2014-06-27 23:50:46 -0700

    added "level generator"

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	List<Animal> animals = new List<Animal>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		List<Animal> newanimals = new List<Animal> ();
+
+		Debug.Log (animals.Count);
+		foreach (Animal a in animals) 
+		{
+			if (a.transform.position.x < -5) {
+				Destroy(a.gameObject);
+			}
+			else
+			{
+				newanimals.Add (a);
+			}
+			animals = newanimals;
+		}
+	}
+
+	public void AddAnimal (Animal a)
+	{
+		animals.Add (a);
+	}
+}
--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class Animal : MonoBehaviour 
 {
-	public bool onFire = false;
-	public float speedX = 3.5f;
-	public float speedZ = 5.0f;
+	public bool onFire;
+	public float speedX;
+	public float speedZ;
+
+	public float runSpeed;
 
 	private Vector3 velocity;
 	private SpriteRenderer spriteRenderer;
+
+	private GameManager gameManager;
 
 	// Use this for initialization
 	void Start () 
@@ -16,6 +20,9 @@ public class Animal : MonoBehaviour
 		velocity = new Vector3();
 		spriteRenderer = this.GetComponent<SpriteRenderer>();
 		//spriteRenderer.color = Color.red;
+
+		gameManager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+		gameManager.AddAnimal (this);
 	}
 	
 	// Update is called once per frame
@@ -24,6 +31,11 @@ public class Animal : MonoBehaviour
 		if (!onFire)
 		{
 			// AI here?
+
+			this.transform.position = new Vector3(this.transform.position.x - runSpeed * Time.deltaTime,
+			                                      this.transform.position.y,
+			                                      this.transform.position.z);
+
 			return;
 		}
 
@@ -41,11 +53,11 @@ public class Animal : MonoBehaviour
 		else
 			velocity.z = 0.0f;
 
-		Debug.Log(velocity);
-
 		this.transform.position = new Vector3(this.transform.position.x + velocity.x,
 				                              this.transform.position.y + velocity.y,
 				                              this.transform.position.z + velocity.z);
+
+
 
 	}
 }
