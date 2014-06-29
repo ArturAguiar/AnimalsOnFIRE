089c911d43f7c13e2c2d86aabc837dde3f7087da
Danny Carpenter <djc423@cornell.edu>
2014-06-29 14:57:32 -0700

    made a crappy title screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+    public Texture backgroundTexture;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("return"))
+        {
+            Application.LoadLevel(1);
+        }
+	}
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+    }
+}
